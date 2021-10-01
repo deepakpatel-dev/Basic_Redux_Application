d4e7bbc2e907462d74ee8ab40d14b6cb5fb3a7bc
deepakpatel-dev <deepak.patel151991@gmail.com>
2021-10-01 09:27:46 +0530

    Setting up a React Application for Redux
    The App will used redux concepts and  it is  a basic application.

---
+++ src/Components.js/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Buy Adibas Shoes</h2>
+            <button className="btn btn-primary my-1 mx-2">-</button>
+             Add to cart
+            <button className="btn btn-primary my-1 mx-2">+</button>
+        </div>
+    )
+}
+
+export default Shop
